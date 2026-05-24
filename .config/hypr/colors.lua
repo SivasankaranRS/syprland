ef8ae0fcd0d169ea0dd072b747a06a6a39bed5d6
SivasankaranRS <sivasankaranradhasoman@gmail.com>
2026-05-24 09:56:59 +0530

    Refactor swaync and awww configurations for improved theme handling and UI consistency
    - Updated awww_init.sh to read theme mode from cache and apply it to Matugen/Waybar.
    - Changed text color in wallpicker.rasi for better visibility.
    - Increased control center width in swaync config for better layout.
    - Removed unused backlight icons from swaync.
    - Added new color definitions in swaync themes for better contrast and aesthetics.
    - Updated CSS styles in swaync for buttons and notifications to enhance user experience.
    - Deleted deprecated central_control and notifications styles in nova-dark theme.
    - Added new colors.css for waybar with consistent theming.
    - Updated .gitignore to exclude temporary files from swaync.

--- .config/hypr/colors.lua
+++ .config/hypr/colors.lua
@@ -1,104 +1,104 @@
 return {
-    image = "/home/sivarch/Pictures/Wallpapers/planet.png",
+    image = "/home/sivarch/Pictures/Wallpapers/anime2.png",
 
-    background = "0xff161217",
+    background = "0xff151313",
 
     error = "0xffffb4ab",
 
     error_container = "0xff93000a",
 
-    inverse_on_surface = "0xff342f35",
+    inverse_on_surface = "0xff323030",
 
-    inverse_primary = "0xff7f4799",
+    inverse_primary = "0xff6a5b5c",
 
-    inverse_surface = "0xffe9e0e8",
+    inverse_surface = "0xffe7e1e1",
 
-    on_background = "0xffe9e0e8",
+    on_background = "0xffe7e1e1",
 
     on_error = "0xff690005",
 
     on_error_container = "0xffffdad6",
 
-    on_primary = "0xff4c1366",
+    on_primary = "0xff3a2d2e",
 
-    on_primary_container = "0xffe9afff",
+    on_primary_container = "0xffad9b9c",
 
-    on_primary_fixed = "0xff310048",
+    on_primary_fixed = "0xff24191a",
 
-    on_primary_fixed_variant = "0xff652e7f",
+    on_primary_fixed_variant = "0xff514344",
 
-    on_secondary = "0xff3e2847",
+    on_secondary = "0xff352f2f",
 
-    on_secondary_container = "0xfff6d5fe",
+    on_secondary_container = "0xfff1e5e5",
 
-    on_secondary_fixed = "0xff281331",
+    on_secondary_fixed = "0xff201a1b",
 
-    on_secondary_fixed_variant = "0xff563e5f",
+    on_secondary_fixed_variant = "0xff4c4545",
 
-    on_surface = "0xffe9e0e8",
+    on_surface = "0xffe7e1e1",
 
-    on_surface_variant = "0xffd0c2d0",
+    on_surface_variant = "0xffd1c3c4",
 
-    on_tertiary = "0xff5f0c37",
+    on_tertiary = "0xff363025",
 
-    on_tertiary_container = "0xffffadca",
+    on_tertiary_container = "0xffa89e8f",
 
-    on_tertiary_fixed = "0xff3e0020",
+    on_tertiary_fixed = "0xff201b11",
 
-    on_tertiary_fixed_variant = "0xff7c264d",
+    on_tertiary_fixed_variant = "0xff4d463a",
 
-    outline = "0xff998d9a",
+    outline = "0xff9a8e8e",
 
-    outline_variant = "0xff4d444f",
+    outline_variant = "0xff4e4445",
 
-    primary = "0xffeab2ff",
+    primary = "0xffd6c2c3",
 
-    primary_container = "0xff51196b",
+    primary_container = "0xff190f10",
 
-    primary_fixed = "0xfff7d8ff",
+    primary_fixed = "0xfff2dedf",
 
-    primary_fixed_dim = "0xffeab2ff",
+    primary_fixed_dim = "0xffd6c2c3",
 
     scrim = "0xff000000",
 
-    secondary = "0xffdbbce4",
+    secondary = "0xffcfc4c4",
 
-    secondary_container = "0xff563e5f",
+    secondary_container = "0xff514a4a",
 
-    secondary_fixed = "0xfff7d8ff",
+    secondary_fixed = "0xffece0e0",
 
-    secondary_fixed_dim = "0xffdbbce4",
+    secondary_fixed_dim = "0xffcfc4c4",
 
     shadow = "0xff000000",
 
-    source_color = "0xff51196b",
+    source_color = "0xff190f10",
 
-    surface = "0xff161217",
+    surface = "0xff151313",
 
-    surface_bright = "0xff3d373e",
+    surface_bright = "0xff3b3938",
 
-    surface_container = "0xff231e24",
+    surface_container = "0xff211f1f",
 
-    surface_container_high = "0xff2d282e",
+    surface_container_high = "0xff2c2929",
 
-    surface_container_highest = "0xff383339",
+    surface_container_highest = "0xff363434",
 
-    surface_container_low = "0xff1f1a20",
+    surface_container_low = "0xff1d1b1b",
 
-    surface_container_lowest = "0xff110d12",
+    surface_container_lowest = "0xff0f0e0e",
 
-    surface_dim = "0xff161217",
+    surface_dim = "0xff151313",
 
-    surface_tint = "0xffeab2ff",
+    surface_tint = "0xffd6c2c3",
 
-    surface_variant = "0xff4d444f",
+    surface_variant = "0xff4e4445",
 
-    tertiary = "0xffffb0cc",
+    tertiary = "0xffd0c5b6",
 
-    tertiary_container = "0xff65123b",
+    tertiary_container = "0xff161108",
 
-    tertiary_fixed = "0xffffd9e4",
+    tertiary_fixed = "0xffede1d1",
 
-    tertiary_fixed_dim = "0xffffb0cc",
+    tertiary_fixed_dim = "0xffd0c5b6",
 
 }
--- .config/hypr/scripts/awww_init.sh
+++ .config/hypr/scripts/awww_init.sh
@@ -2,7 +2,7 @@
 
 CACHE_FILE="$HOME/.cache/current_wallpaper"
 DEFAULT_WALLPAPER="$HOME/Pictures/wallpapers/anime1.jpg" # Fallback if cache is empty
-
+THEME_MODE=$(cat "$HOME/.cache/awww/theme_mode.txt")
 # 1. Start the core engine daemon
 awww-daemon &
 
@@ -11,13 +11,14 @@ sleep 0.5
 
 # 3. Read cache or fall back to default image path
 if [ -f "$CACHE_FILE" ]; then
-    WALLPAPER=$(cat "$CACHE_FILE")
+  WALLPAPER=$(cat "$CACHE_FILE")
 else
-    WALLPAPER="$DEFAULT_WALLPAPER"
+  WALLPAPER="$DEFAULT_WALLPAPER"
 fi
 
 # 4. Restore the image and push colors to Matugen/Waybar
 if [ -f "$WALLPAPER" ]; then
-    awww img "$WALLPAPER" --transition-type none
-    matugen image "$WALLPAPER"
+  awww img "$WALLPAPER" --transition-type none
+  matugen image "$WALLPAPER" --prefer darkness --type scheme-content -m $THEME_MODE
 fi
+
